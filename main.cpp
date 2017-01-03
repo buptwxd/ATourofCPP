25bccf2efae238693918a00c62be7d4367d6ab66
buptwxd <buptwxd@gmail.com>
2017-01-03 20:13:17 +0800

    Add first example program

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "This is Alex's first C++ program" << std::endl;
+    std::cout << "Welcome back" << std::endl;
+    return 0;
+}
